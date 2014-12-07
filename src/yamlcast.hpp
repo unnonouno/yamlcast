c56e7e92413182055435cf3f6d031a6314faa15d
Yuya Unno <unnonouno@gmail.com>
2014-12-08 01:37:14 +0900

    Add headers

--- src/yamlcast.hpp
+++ src/yamlcast.hpp
@@ -3,5 +3,10 @@
 
 #include "./yamlcast/exception.hpp"
 #include "./yamlcast/iarchive.hpp"
+#include "./yamlcast/mapping.hpp"
+#include "./yamlcast/scalar.hpp"
+#include "./yamlcast/sequence.hpp"
+#include "./yamlcast/util.hpp"
+#include "./yamlcast/yaml.hpp"
 
 #endif  // YAMLCAST_HPP_EAA5E9B1_923F_439B_8EAE_0B8808AEE23E_
